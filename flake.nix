4ccda87064980e2405f90af708372978c4a9e74e
mst-mkt <mail@keito.dev>
2026-06-08 15:30:17 +0900

    init flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "mst-mkt/serenity-emoji";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            pnpm
+          ];
+
+          shellHook = "echo \"node $(node --version), pnpm $(pnpm --version)\"";
+        };
+      });
+    };
+}
